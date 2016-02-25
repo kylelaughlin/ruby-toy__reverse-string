161c4bcc2779a86b5fc720a322417d52029ceb3b
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-02-25 18:44:27 +0000

    Reverse String Done

--- lib/reverse_string.rb
+++ lib/reverse_string.rb
@@ -1,5 +1,26 @@
 def reverse_characters(str)
+    string_array = str.split("")
+    reverse_string = ""
+    i = string_array.length - 1
+    while i >= 0 do
+        reverse_string = reverse_string + string_array[i]
+        i -= 1
+    end
+    return reverse_string
 end
 
 def reverse_words(str)
+    string_array = str.split(" ")
+    reverse_string = ""
+    i = string_array.length - 1
+    while i >= 0 do
+        if i == 0
+            reverse_string = reverse_string + string_array[i]
+            i -= 1
+        else
+            reverse_string = reverse_string + "#{string_array[i]} "
+            i -= 1
+        end
+    end
+    return reverse_string
 end
